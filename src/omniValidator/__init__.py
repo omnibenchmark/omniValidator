c0b4e645f30b10d1883628d3aa0310fd7ba569a9
Anthony Sonrel <anthony.sonrel@uzh.ch>
2022-10-30 17:33:57 +0100

    init

---
+++ src/omniValidator/__init__.py
@@ -0,0 +1,8 @@
+def main():
+    """Entry point for the application script"""
+    print("Call your main application code here")
+
+# TODO: import eventual subscripts with: 
+#import pkg.mod1, pkg.mod2
+# so that for example >>> pkg.mod1.foo() works
+# or import *
