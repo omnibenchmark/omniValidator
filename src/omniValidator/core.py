b6a269239c8915f65a682bb6211ce0b80b9450f4
Anthony Sonrel <anthony.sonrel@uzh.ch>
2022-11-04 17:00:36 +0100

    new schemas and requirements.

--- src/omniValidator/core.py
+++ src/omniValidator/core.py
@@ -1,5 +1,18 @@
+import json 
 
 def schema_validate():
     """
     """
     return("ok")
+
+
+def validateJSON(jsonData):
+    """
+    Returns False if input can not be decoded as JSON. 
+    """
+    try:
+        json.loads(jsonData)
+    except ValueError as err:
+        return False
+    return True
+
